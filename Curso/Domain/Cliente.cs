449dbfda2e985ae659f87c372f6a313e31c66dbc
Davi <davi.freitas@useall.com.br>
2022-03-10 15:56:20 -0300

    created migration + sql

--- Curso/Domain/Cliente.cs
+++ Curso/Domain/Cliente.cs
@@ -1,9 +1,13 @@
 namespace CursoEFCore.Domain
 {
+    //[Table("Clientes")]Data anotation para nome diferente de tabela
     public class Cliente
     {
+        //[Key] Data anotation pra PK
         public int Id { get; set; }
+        //[Required] Data anotation para requerido
         public string Nome { get; set; }
+        // [Column("Phone")]Data anotation para nome diferente no banco
         public string Telefone { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
